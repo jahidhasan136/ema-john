e27dd1c2bdc97bc5a41e0359eac5dd44548b7609
Jahid Hasan <jh3495689@gmail.com>
2023-04-28 13:21:38 +0600

    added a product review cart icon

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRightFromBracket, faBuildingCircleArrowRight, faRightFromBracket, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 
 const Product = (props) => {
@@ -10,21 +10,24 @@ const Product = (props) => {
 
 
     return (
-        <div>
             <div className='product'>
                 <img src={img} alt="" />
                 <div className='product-info'>
                     <h6 className='product-name'>{name}</h6>
                     <p>Price: ${price}</p>
                     <p>Manufacturer: {seller}</p>
-                    <p>Rating: {ratings} Stars</p>
                 </div>
-                <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
-                    Add to Cart
-                    <FontAwesomeIcon icon={faShoppingCart} />
-                </button>
+                <div className='btn-cart '>
+                    <div onClick={() => handleAddToCart(props.product)} className='cart-item'>
+                        <p>Add to Cart</p>
+                        <FontAwesomeIcon icon={faShoppingCart} />
+                    </div>
+                    <div className='review'>
+                        <p>Review</p>
+                        <FontAwesomeIcon icon={faRightFromBracket}/>
+                    </div>
+                </div>
             </div>
-        </div>
     );
 };
 
